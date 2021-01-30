f95935b5649ba09c68628e4253d76df8e38339a9
msr1013 <rawalsinghmanish@gmail.com>
2021-01-30 17:54:12 +0545

    note-reducer

---
+++ src/noteReducer.js
@@ -0,0 +1,44 @@
+
+const noteReducer = (state = [], action) => {
+    if (action.type === 'NEW_NOTE') {
+        state.push(action.data)
+        return state
+    }
+
+    return state
+}
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    data: {
+        content: 'this app state is in redux store',
+        important: true,
+        id: 1
+    }
+})
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    data: {
+        content: 'state changes are made with actions',
+        important: false,
+        id: 2
+    }
+})
+
+const App = () => {
+    return (
+        <div>
+            <ul>
+                {store.getState().map(note =>
+                    <li key={note.id}>
+                        {note.content}<strong>{note.important ? 'important' : ''}</strong>
+
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
+}

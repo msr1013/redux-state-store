995cc14d25a571b3a8a273bcd4d0fee71f9b2f81
msr1013 <rawalsinghmanish@gmail.com>
2021-01-30 17:09:54 +0545

    counter-ui-state-store-redux

--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,24 @@
 
-const counterReducer = (state, action) => {
-    if (action.type === 'INCREMENT') {
-        return state + 1
-    } else if (action.type === 'DECREMENT') {
-        return state - 1
-    } else if (action.type === 'ZERO') {
-        return 0
-    }
 
-    return state
-}
+// const counterReducer = (state = 0, action) => {
+//     switch (action.type) {
+//         case 'INCREMENT':
+//             return state + 1
+//         case 'DECREMENT':
+//             return state - 1
+//         case 'ZERO':
+//             return 0
+//         default:
+//             return state
+//     }
+// }
+
+// const store = createStore(counterReducer)
+// console.log(store.getState())
+// store.dispatch({ type: 'INCREMENT' })
+// store.dispatch({ type: 'INCREMENT' })
+// store.dispatch({ type: 'INCREMENT' })
+// console.log(store.getState())
+// store.dispatch({ type: 'ZERO' })
+// store.dispatch({ type: 'DECREMENT' })
+// console.log(store.getState())
